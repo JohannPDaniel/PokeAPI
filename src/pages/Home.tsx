75700aad124d441ea48b3de6ce4c865dc345527f
Johann Patricio Daniel <156346733+JohannPDaniel@users.noreply.github.com>
2024-12-19 13:41:49 -0300

    arrumando .Env

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,28 @@
 import { Box } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../store/hook';
 import { useEffect } from 'react';
-import { getPokemonAsyncThunk } from "../store/modules/getPokemonSlice/getPokemon.action";
+import { getPokemonAsyncThunk } from '../store/modules/getPokemonSlice/getPokemon.action';
 
 export const Home = () => {
 	const dispatch = useAppDispatch();
-	const getPokemon = useAppSelector( ( state ) => state.getPokemon );
+	const getPokemon = useAppSelector((state) => state.getPokemon);
 
-	useEffect( () => {
-		dispatch( getPokemonAsyncThunk() );
-	}, [ dispatch ] );
+	useEffect(() => {
+		dispatch(getPokemonAsyncThunk());
+	}, [dispatch]);
 
 	return (
 		<Box>
-			{ getPokemon.results && getPokemon.results.length > 0 ? (
-				getPokemon.results.map( ( pok ) => <h1 key={ pok.name }>{ pok.name }</h1> )
+			{getPokemon.results && getPokemon.results.length > 0 ? (
+				getPokemon.results.map((pok) => (
+					<>
+						<h1 key={pok.name}>{pok.name}</h1>
+						<p>{pok.url}</p>
+					</>
+				))
 			) : (
 				<p>Carregando dados...</p>
-			) }
+			)}
 		</Box>
 	);
 };
